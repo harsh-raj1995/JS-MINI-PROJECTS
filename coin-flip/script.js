cf7daab26760bdb882d5baec5ab6ef3e85620e93
Harsh Raj <rajharsha806@gmail.com>
2026-02-24 12:41:53 +0530

    css left

---
+++ coin-flip/script.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector("button")
+let p = document.querySelector("p")
+let random=0;
+
+btn.addEventListener("click",()=>{
+  alert('LOADING CLICK TO VIEW RESULT')
+  random=Math.random()*10
+  if(random<5){
+    p.innerHTML = '<img src="https://www.pikpng.com/pngl/b/176-1762234_coin-clipart-quarter-tail-cash-png-download.png" alt="HEADS" width="200px">' + '<BR>its tails';
+
+  }else{
+    p.innerHTML='<img src="https://www.pngarts.com/files/3/Silver-Coin-Transparent-Background-PNG.png" alt="Classic Head $2 - Coin Front Png@nicepng.com" width="200px">'
+    +'<BR>its heads';
+  }
+})
